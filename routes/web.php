3410b3303afbe456d223043ca020d5b870312df3
Y2K <yeyintkyaw300@gmail.com>
2021-12-16 17:21:41 -0800

    database setup, adminlte setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DishesController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\OrderController::class, 'index'])->name('order');
+Route::resource('/dish', App\Http\Controllers\DishesController::class);
